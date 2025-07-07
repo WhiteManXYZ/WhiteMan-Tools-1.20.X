e2f7dbe72e3c0e001e7a3658ba5a013ee4b3dac3
Whiteman <vladimir.gavrilyuk10@gmail.com>
2025-07-07 20:21:06 +0200

    New changes

--- src/main/java/net/whiteman/whitemantools/WhiteManToolsMod.java
+++ src/main/java/net/whiteman/whitemantools/WhiteManToolsMod.java
@@ -2,6 +2,7 @@ package net.whiteman.whitemantools;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.whiteman.whitemantools.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -25,6 +27,8 @@ public class WhiteManToolsMod {
     public WhiteManToolsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.ITEMS.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -37,7 +41,9 @@ public class WhiteManToolsMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.UNKNOWN_COMPOUND);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/whiteman/whitemantools/item/ModItems.java
+++ src/main/java/net/whiteman/whitemantools/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.whiteman.whitemantools.item;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.whiteman.whitemantools.WhiteManToolsMod;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, WhiteManToolsMod.MOD_ID);
+
+    public static final RegistryObject<Item> UNKNOWN_COMPOUND = ITEMS.register("unknown_compound",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
