1556f8501e4908ff9d53b0a946465512f1de9c2a
Whiteman <vladimir.gavrilyuk10@gmail.com>
2025-07-07 21:15:00 +0200

    New changes

--- src/main/java/net/whiteman/whitemantools/WhiteManToolsMod.java
+++ src/main/java/net/whiteman/whitemantools/WhiteManToolsMod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.whiteman.whitemantools.item.ModCreativeModTabs;
 import net.whiteman.whitemantools.item.ModItems;
 import org.slf4j.Logger;
 
@@ -26,6 +27,8 @@ public class WhiteManToolsMod {
 
     public WhiteManToolsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModTabs.register(modEventBus);
 
         ModItems.ITEMS.register(modEventBus);
 
---
+++ src/main/java/net/whiteman/whitemantools/item/ModCreativeModTabs.java
@@ -0,0 +1,27 @@
+package net.whiteman.whitemantools.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.whiteman.whitemantools.WhiteManToolsMod;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, WhiteManToolsMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> WHITEMAN_TOOLS_TAB = CREATIVE_MODE_TABS.register("whiteman_tools_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.UNKNOWN_COMPOUND.get()))
+                    .title(Component.translatable("creativetab.whiteman_tools_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.UNKNOWN_COMPOUND.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
