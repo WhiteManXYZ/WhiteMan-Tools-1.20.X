03cbe3a1397564a5be53c0d7f40a8fa2623a7440
WhiteManXYZ <vladimir.gavrilyul10@gmail.com>
2026-03-07 11:46:18 +0100

    - Drop system added

--- src/main/java/net/whiteman/biosanity/block/custom/neoplasm/NeoplasmRotBlock.java
+++ src/main/java/net/whiteman/biosanity/block/custom/neoplasm/NeoplasmRotBlock.java
@@ -13,13 +13,18 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.Vec3;
 import net.whiteman.biosanity.block.entity.NeoplasmRotBlockEntity;
 import net.whiteman.biosanity.item.ModItems;
 import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.Nullable;
+import java.util.List;
+
 public class NeoplasmRotBlock extends NeoplasmBlock implements EntityBlock {
     public static final EnumProperty<NeoplasmResourceType> TYPE = EnumProperty.create("type", NeoplasmResourceType.class);
-    private static final double DECREASE_DROP_CHANCE_RATE = 0.2;
 
     public NeoplasmRotBlock(Properties properties) {
         super(properties);
@@ -40,7 +45,8 @@ public class NeoplasmRotBlock extends NeoplasmBlock implements EntityBlock {
     @Override
     public void randomTick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull RandomSource pRandom) {
         if (pLevel.getBlockEntity(pPos) instanceof NeoplasmRotBlockEntity be) {
-            be.decreaseChance(DECREASE_DROP_CHANCE_RATE);
+            if (be.getDropChance() > 0)
+                be.decreaseChance();
         }
     }
 
@@ -50,14 +56,27 @@ public class NeoplasmRotBlock extends NeoplasmBlock implements EntityBlock {
     }
 
     @Override
-    public void playerDestroy(@NotNull Level level, @NotNull Player player, @NotNull BlockPos pos, @NotNull BlockState state, @org.jetbrains.annotations.Nullable BlockEntity pBlockEntity, @NotNull ItemStack pTool) {
-        if (pBlockEntity instanceof NeoplasmRotBlockEntity be) {
+    public void playerDestroy(@NotNull Level level, @NotNull Player player, @NotNull BlockPos pos, @NotNull BlockState state, @Nullable BlockEntity pBlockEntity, @NotNull ItemStack pTool) {
+        if (pBlockEntity instanceof NeoplasmRotBlockEntity be && level instanceof ServerLevel) {
             BlockState original = be.getOriginalState();
 
             if (!player.isCreative()) {
                 double currentChance = be.getDropChance();
-                if (level.random.nextFloat() < currentChance && !original.isAir()) {
-                    Block.popResource(level, pos, new ItemStack(original.getBlock().asItem()));
+                if (level.random.nextFloat() < currentChance && (!original.isAir() && player.hasCorrectToolForDrops(original))) {
+                    // We want the broken block to use a drop table, not the block itself
+                    // If player has correct tool for block
+                    LootParams.Builder builder = new LootParams.Builder((ServerLevel) level)
+                            .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
+                            .withParameter(LootContextParams.BLOCK_STATE, original)
+                            .withParameter(LootContextParams.TOOL, player.getMainHandItem())
+                            .withParameter(LootContextParams.THIS_ENTITY, player);
+
+                    List<ItemStack> drops = original.getBlock().getDrops(original, builder);
+
+                    for (ItemStack stack : drops) {
+                        Block.popResource(level, pos, stack);
+                    }
+
                 } else {
                     Block.popResource(level, pos, new ItemStack(ModItems.NEOPLASM_ROT.get()));
                 }
