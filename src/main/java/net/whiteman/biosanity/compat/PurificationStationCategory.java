2404e6a7f493d5636b76b6f0d9f5e2feaa6d9b9c
WhiteManXYZ <vladimir.gavrilyuk10@gmail.com>
2026-03-10 20:51:21 +0100

    - JEI purification recipe completed

--- src/main/java/net/whiteman/biosanity/compat/PurificationStationCategory.java
+++ src/main/java/net/whiteman/biosanity/compat/PurificationStationCategory.java
@@ -12,6 +12,8 @@ import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.chat.Component;
@@ -25,7 +27,7 @@ import org.jetbrains.annotations.Nullable;
 
 public class PurificationStationCategory implements IRecipeCategory<PurificationStationRecipe> {
     public static final ResourceLocation UID = new ResourceLocation(BiosanityMod.MOD_ID, "purification");
-    public static final ResourceLocation TEXTURE = new ResourceLocation(BiosanityMod.MOD_ID,
+    public static final ResourceLocation PURIFICATION_STATION_TEXTURE = new ResourceLocation(BiosanityMod.MOD_ID,
             "textures/gui/purification_station_block_gui.png");
 
     public static final RecipeType<PurificationStationRecipe> PURIFICATION_TYPE =
@@ -33,33 +35,41 @@ public class PurificationStationCategory implements IRecipeCategory<Purification
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final IDrawable pressureIcon;
+    private final IDrawable fuelIcon;
+    private final IDrawable modifierIcon;
     private final IDrawableAnimated animatedArrow;
     private final IDrawableAnimated bubbles;
 
     public PurificationStationCategory(IGuiHelper helper) {
-        this.background = helper.createDrawable(TEXTURE, 48, 16, 121, 53);
+        this.background = helper.createDrawable(PURIFICATION_STATION_TEXTURE, 48, 16, 121, 58);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.PURIFICATION_STATION_BLOCK.get()));
 
-        IDrawableStatic staticArrow = helper.createDrawable(TEXTURE, 176, 0, 24, 17);
+        IDrawableStatic staticArrow = helper.createDrawable(PURIFICATION_STATION_TEXTURE, 176, 0, 24, 17);
         this.animatedArrow = helper.createAnimatedDrawable(staticArrow, 400, IDrawableAnimated.StartDirection.LEFT, false);
 
         ITickTimer bubblesTickTimer = new PurificationBubblesTickTimer(helper);
-        bubbles = helper.drawableBuilder(TEXTURE, 176, 29, 11, 11)
+        bubbles = helper.drawableBuilder(PURIFICATION_STATION_TEXTURE, 176, 29, 11, 11)
                 .buildAnimated(bubblesTickTimer, IDrawableAnimated.StartDirection.BOTTOM);
+
+        this.pressureIcon = helper.createDrawable(PURIFICATION_STATION_TEXTURE, 205, 51, 17, 17);
+        this.fuelIcon = helper.createDrawable(PURIFICATION_STATION_TEXTURE, 176, 17, 12, 4);
+        this.modifierIcon = helper.createDrawable(PURIFICATION_STATION_TEXTURE, 176, 21, 19, 8);
     }
 
+
     @Override
-    public RecipeType<PurificationStationRecipe> getRecipeType() {
+    public @NotNull RecipeType<PurificationStationRecipe> getRecipeType() {
         return PURIFICATION_TYPE;
     }
 
     @Override
-    public Component getTitle() {
+    public @NotNull Component getTitle() {
         return Component.translatable("jei.block.action.biosanity.purification_translatable");
     }
 
     @Override
-    public IDrawable getBackground() {
+    public @NotNull IDrawable getBackground() {
         return this.background;
     }
 
@@ -75,14 +85,25 @@ public class PurificationStationCategory implements IRecipeCategory<Purification
     }
 
     @Override
-    public void draw(PurificationStationRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+    public void draw(@NotNull PurificationStationRecipe recipe, @NotNull IRecipeSlotsView recipeSlotsView, @NotNull GuiGraphics guiGraphics, double mouseX, double mouseY) {
         IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
-        this.animatedArrow.draw(guiGraphics, 44, 24);
+        this.animatedArrow.draw(guiGraphics, 44, 23);
         bubbles.draw(guiGraphics, 6, 35);
+        pressureIcon.draw(guiGraphics, 104, 0);
+        fuelIcon.draw(guiGraphics, 2, 48);
+        modifierIcon.draw(guiGraphics, 22, 1);
+
+        // FIXME: Should use getPurificationSeconds instead hardcoded int
+        int purificationSeconds = 20;
+        Component timeString = Component.translatable("gui.jei.category.smelting.time.seconds", purificationSeconds);
+        Minecraft minecraft = Minecraft.getInstance();
+        Font fontRenderer = minecraft.font;
+        int stringWidth = fontRenderer.width(timeString);
+        guiGraphics.drawString(fontRenderer, timeString, getWidth() - stringWidth, 49, 0xFF808080, false);
     }
 
     private static class PurificationBubblesTickTimer implements ITickTimer {
-        private static final int[] BUBBLE_LENGTHS = new int[]{10, 5, 0};
+        private static final int[] BUBBLE_LENGTHS = new int[]{11, 5, 0};
         private final ITickTimer internalTimer;
 
         public PurificationBubblesTickTimer(IGuiHelper guiHelper) {
@@ -101,4 +122,3 @@ public class PurificationStationCategory implements IRecipeCategory<Purification
         }
     }
 }
-
