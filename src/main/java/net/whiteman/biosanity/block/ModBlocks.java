00c6a21483f0b1d801323622409cbc78943df3f9
WhiteManXYZ <vladimir.gavrilyuk10@gmail.com>
2026-03-28 15:26:31 +0100

    - Neoplasm blocks inherit expanded

--- src/main/java/net/whiteman/biosanity/block/ModBlocks.java
+++ src/main/java/net/whiteman/biosanity/block/ModBlocks.java
@@ -36,20 +36,20 @@ public class ModBlocks {
             ));
 
     public static final RegistryObject<Block> NEOPLASM_BLOCK = registerBlock("neoplasm_block",
-            () -> new NeoplasmCoreBlock(BlockBehaviour.Properties.copy(Blocks.SLIME_BLOCK)
+            () -> new NeoplasmCoreBlock(BlockBehaviour.Properties.of()
                     .strength(0.3f, 9f)
                     .randomTicks()
             ));
 
     public static final RegistryObject<Block> NEOPLASM_ROT_BLOCK = registerBlock("neoplasm_rot_block",
-            () -> new NeoplasmRotBlock(BlockBehaviour.Properties.copy(Blocks.SLIME_BLOCK)
+            () -> new NeoplasmRotBlock(BlockBehaviour.Properties.of()
                     .strength(1.5f, 2f)
                     .ignitedByLava()
                     .randomTicks()
             ));
 
     public static final RegistryObject<Block> NEOPLASM_VEIN_BLOCK = registerBlock("neoplasm_vein_block",
-            () -> new NeoplasmVeinBlock(BlockBehaviour.Properties.copy(Blocks.SLIME_BLOCK)
+            () -> new NeoplasmVeinBlock(BlockBehaviour.Properties.of()
                     .strength(2.5f, 6f)
                     .randomTicks()
             ));
--- src/main/java/net/whiteman/biosanity/block/custom/neoplasm/NeoplasmBlock.java
+++ src/main/java/net/whiteman/biosanity/block/custom/neoplasm/NeoplasmBlock.java
@@ -1,7 +1,14 @@
 package net.whiteman.biosanity.block.custom.neoplasm;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MapColor;
 
 public class NeoplasmBlock extends Block {
-    public NeoplasmBlock(Properties pProperties) { super(pProperties); }
+    public NeoplasmBlock(Properties pProperties) { super(pProperties
+            .strength(1.0F, 9.0F)
+            .friction(0.8F)
+            .sound(SoundType.SLIME_BLOCK)
+            .mapColor(MapColor.TERRACOTTA_RED));
+    }
 }
