f23b02a38fc215afd58499b03b10d0efafda2882
WhiteManXYZ <vladimir.gavrilyuk10@gmail.com>
2026-04-12 20:27:30 +0200

    - Shuffle method added (directions array)

--- src/main/java/net/whiteman/biosanity/world/neoplasm/common/NeoplasmConstants.java
+++ src/main/java/net/whiteman/biosanity/world/neoplasm/common/NeoplasmConstants.java
@@ -1,8 +1,18 @@
 package net.whiteman.biosanity.world.neoplasm.common;
 
 import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+
+import java.util.Collection;
 
 public class NeoplasmConstants {
     public static final Direction[] DIRECTIONS = Direction.values();
     public static final Direction[] HORIZONTAL_DIRECTIONS = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
+
+    /** Shuffles directions array,
+     * useful when needed to randomize each direction checking
+     * @return Shuffled collection of directions */
+    public static Collection<Direction> getShuffledDirections(RandomSource random) {
+        return Direction.allShuffled(random);
+    }
 }
